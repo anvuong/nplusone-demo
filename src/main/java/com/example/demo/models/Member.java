50fc0955d0c216b909e9dd78be6eeb600d531e5b
Vuong Hong An <anvh87@gmail.com>
2020-05-16 16:15:58 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/models/Member.java
@@ -0,0 +1,35 @@
+package com.example.demo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name= "members")
+public class Member {
+  @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="member_id")
+    private Long id;
+
+    @Column(name="first_name")
+    @NotEmpty(message="* Please Enter First Name")
+    private String firstName;
+
+    @Column(name="last_name")
+    @NotEmpty(message="* Please Enter Last Name")
+    private String lastName;
+
+    @Email(message="* Please Enter Valid Email Address")
+    @NotEmpty(message=" * Please Provide Email Address")
+    @Column(name="email")
+    private String email;
+}
